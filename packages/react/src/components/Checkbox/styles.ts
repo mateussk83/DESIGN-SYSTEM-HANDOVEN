5a6b1e776a2752266d9d118bb81a2cc3e3b06473
mateussk83 <mateussk83@gmail.com>
2023-06-04 11:28:13 -0300

    fix: reformed checkbox

--- packages/react/src/components/Checkbox/styles.ts
+++ packages/react/src/components/Checkbox/styles.ts
@@ -5,7 +5,6 @@ export const CheckboxContainer = styled(Checkbox.Root, {
   all: 'unset',
   width: '$6',
   height: '$6',
-  backgroundColor: '$gray900',
   borderRadius: '$xs',
   lineHeight: 0,
   cursor: 'pointer',
@@ -13,15 +12,15 @@ export const CheckboxContainer = styled(Checkbox.Root, {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  border: '2px solid $gray900',
+  border: '2px solid $red500',
   overflow: 'hidden',
 
   '&[data-state="checked"]': {
-    backgroundColor: '$ignite300',
+    backgroundColor: '$red500',
   },
 
   '&:focus, &[data-state="checked"]': {
-    border: '2px solid $ignite300',
+    border: '2px solid $red500',
   },
 })
 
--- packages/react/src/components/Text.tsx
+++ packages/react/src/components/Text.tsx
@@ -5,7 +5,7 @@ export const Text = styled('p', {
   fontFamily: '$default',
   lineHeight: '$base',
   margin: 0,
-  color: '$gray100',
+  color: '$black',
 
   variants: {
     size: {
